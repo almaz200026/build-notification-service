80734de7348aeb6e3e22afc9cee5d8cbd83c2a5d
Almaz <almaz200026>
2021-11-23 10:16:30 +0600

    init project

---
+++ src/main/java/app/mdocs/m11s/build/BuildNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package app.mdocs.m11s.build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/mdocs/m11s/build/BuildNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.mdocs.m11s.build;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
